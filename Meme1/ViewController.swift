54b3922b16d1b48763386e10ee5dbcedee99af9c
Youngsun Paik <youngsunpaik@gmail.com>
2016-03-21 01:30:20 -0700

    Initial Commit

---
+++ Meme1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme1
+//
+//  Created by Youngsun Paik on 3/21/16.
+//  Copyright © 2016 Youngsun Paik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

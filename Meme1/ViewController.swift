f618c46eab96c87f6d1b1c734323920eb633092b
Youngsun Paik <youngsunpaik@gmail.com>
2016-03-23 01:42:40 -0700

    added text attributes for impact

--- Meme1/ViewController.swift
+++ Meme1/ViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topTextField.text = "TOP"
+        topTextField.textAlignment = .Center
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.textAlignment = .Center
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
     }
+    
     
     override func viewWillAppear(animated: Bool) {
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) // enables camera if camera is available.
@@ -46,6 +60,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        topTextField.text = ""
+        bottomTextField.text = ""
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return true
+    }
+    
+//    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+//                // Figure out what the new text will be, if we return true
+//        var newText: NSString = textField.text!
+//        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+//        
+//                // hide the label if the newText will be an empty string
+//        self.topTextField.hidden = (newText.length == 0)
+//        self.bottomTextField.hidden = (newText.length == 0)
+//        
+//                // Write the length of newText into the label
+//        self.topTextField.text = String(newText.length)
+//        self.bottomTextField.text = String(newText.length)
+//        
+//                // returning true gives the text field permission to change its text
+//        return true;
+//    }
+
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: 0.8
+    ]
     
     
 }
